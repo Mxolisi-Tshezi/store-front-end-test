5f2053b83a81eebdabf7a6d6957d2c240eecae49
Mxolisi Tshezi <nyawose57@gmail.com>
2025-04-03 18:36:20 +0200

    Deployment to actions

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone", // Optimize for container deployments
-  swcMinify: true,
-  // Configure any other options as needed for your application
-};
-
-module.exports = nextConfig;
