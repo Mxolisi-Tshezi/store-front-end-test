85920cf805a6a6841dde162b792402091dffb67b
Mxolisi Tshezi <nyawose57@gmail.com>
2025-04-04 11:52:22 +0200

    app/admin error

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+
+export default function UsersPage() {
+    return (
+        <div>
+            <h1>Users</h1>
+            {/* Your add products form and functionality here */}
+        </div>
+    );
+}
