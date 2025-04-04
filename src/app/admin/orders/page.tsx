63bd77183b9ea7ec37f3c9bb48638bf877c1a3d4
Mxolisi Tshezi <nyawose57@gmail.com>
2025-04-04 11:29:20 +0200

    app/admin error

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+
+export default function ordersPage() {
+    return (
+        <div>
+            <h1>Add orders</h1>
+            {/* Your add products form and functionality here */}
+        </div>
+    );
+}
