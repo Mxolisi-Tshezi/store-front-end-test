6cbe2ef09c5252aed50f554cac4b7de580784dba
Mxolisi Tshezi <nyawose57@gmail.com>
2025-04-04 10:50:57 +0200

    pages fix

---
+++ src/app/(main)/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Welcome to the products page. Browse our collection below!</p>
+        </div>
+    );
+};
+
+export default ContactPage;
---
+++ src/app/(main)/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Welcome to the products page. Browse our collection below!</p>
+        </div>
+    );
+};
+
+export default ProductsPage;
